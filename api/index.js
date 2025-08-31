61ef91d1b9cffe6f3a7523a7de406191448ab084
ansubanerjee <banner1891000@gmail.com>
2025-08-31 22:39:31 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).send("✅ API is running. Use POST /api/ingestTelemetry");
-};
+export default function handler(req, res) {
+  res.status(200).json({ message: "Telemetry API is live 🚀" });
+}
